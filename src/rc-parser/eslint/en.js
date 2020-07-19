2b9cb59cb8ff147f951c79b83f7e4ffcbf31720b
xoyljlj <xoyljlj@126.com>
2020-07-19 16:33:46 +0800

    【新增】解析器模块，并新增eslint解析

---
+++ src/rc-parser/eslint/en.js
@@ -0,0 +1,21 @@
+export const parser = innerHTML => {
+  const innerHTMLJSON = innerHTML
+    .replace(/^/, '[')
+    .replace(new RegExp('<tr[^>]*>', 'g'), '{')
+    .replace(new RegExp('</tr>', 'g'), '},')
+    .replace(new RegExp('<td><span title="recommended" aria-label="recommended" class="glyphicon glyphicon-ok"></span></td>', 'g'), '"recommended":true,')
+    .replace(new RegExp('<td><span title="fixable" aria-label="fixable" class="glyphicon glyphicon-wrench"></span></td>', 'g'), '"fixable":true,')
+    .replace(new RegExp('<td markdown="1"><a href="([^"]*)">\\1</a></td>', 'g'), '"name":"$1",')
+    .replace(new RegExp('<td markdown="1">([^<]+)</td>', 'g'), (match, p1) => `"desc":"${p1.replace(/"/g, '\'')}"`)
+    .replace(new RegExp('<td></td>', 'g'), '')
+    .replace(/,$/, ']')
+
+  const obj = JSON.parse(innerHTMLJSON)
+  const props = obj.reduce((acc, curr) => {
+    acc[curr.name] = { ...curr }
+    delete acc[curr.name].name
+    return acc
+  }, {})
+
+  return props
+}
---
+++ src/rc-parser/eslint/index.js
@@ -0,0 +1,2 @@
+export { parser as enParser } from './en'
+export { parser as cnParser } from './cn'
